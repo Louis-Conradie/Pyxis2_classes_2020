6811cd094805cf9106c0ac4d4d33a946749edafb
Louis-Conradie <75941680+Louis-Conradie@users.noreply.github.com>
2020-12-17 15:38:39 +0200

    Create GFF_parser.py

--- GFF_parser.py
+++ GFF_parser.py
@@ -1,20 +1,33 @@
 import re
 from itertools import groupby
 import argparse
+
 parser = argparse.ArgumentParser(description='Pyxis detects clusters of regulated genes and returns the confidence')
 parser.add_argument('-v', metavar='v', type=str, help="GFF file") #GFF file
 parser.add_argument('-s', metavar='s', type=str, help="List of regulated genes") #file with upregulated genes
 parser.add_argument('-w', metavar='w', type=str, nargs='?', default='None', const='None', help='Statistical test used to detect clusters')
 parser.add_argument('-i', metavar='i', type=str, nargs='?', default='ID=gene:', const='ID=gene:', help='Search string used to identify Genes in GFF file (default: ID=gene:)')
-parser.add_argument('-k', metavar='k', type=str, nargs='?', default='Name=', const='Name=', help='Search string used to identify Gene name in GFF file (default: Name=)')
-parser.add_argument('-p', metavar='p', type=int, nargs='?', default=0.01, const=0.01, help='P-value used to make statistical test more rigorous (default: 0.05)')
+parser.add_argument('-k', metavar='k', type=int, nargs='?', help='Search string used to identify Gene name in GFF file (default: Name=)')
+parser.add_argument('-p', metavar='p', type=float, nargs='?', default=0.01, const=0.01, help='Lower P-value used to make statistical test more rigorous (default: 0.01)')
+parser.add_argument('-q', metavar='q', type=float, nargs='?',default=0.05, const=0.05, help='Maximum p-value to make statistical test more rigorous (default:0.05)')
 parser.add_argument('-l', metavar='l', type=int, nargs='?', default=5, const=5, help='Minimum window to detect gene clusters (default: 5)')
 args = parser.parse_args()
+
+if args.k == 1:
+    type_gene = str("Name=")
+    new_gene = str("Protein coding genes")
+    
+if args.k == 0:
+    type_gene = str("")
+    new_gene = str("Protein coding, dubious and merged ORFs")
+
+
 GFF_file = args.v
 files = args.s
 idstring = args.i
 sig_value = args.p
 minimum_window = args.l
+
 class GFF_parser():
     def __init__(self):
         self.chromosome_list = []
@@ -65,8 +78,11 @@ class GFF_parser():
                 if name in line:
                     words = line.split()
                     if words[2] == "gene":
-                        if args.k in words[8]:
+                        if type_gene in words[8]:
                             lines.append(words)
+                        else:
+                            if type_gene == "":
+                                lines.append(words)
         header = [list(g) for k, g in groupby(lines, key=lambda x: x[0])]
         #create list of lists called header and group each lines into lists based on the first element (chromosome number) of the split lines
         genes = [[x[3:5] for x in y] for y in header]
@@ -89,8 +105,12 @@ class GFF_parser():
                     words = line.split()
                     if words[2] == "gene":
                         #if third element contains the name gene
-                        if args.k in words[8]:
+                        if type_gene in words[8]:
                             lines.append(words)
+                        else:
+                            if type_gene == "":
+                                lines.append(words)
+                                
                         #append the split lines to empty list called lines
         names = []
         gene_names = []
